96c006decf8c82f423eb1565730a8484a681772a
Zhi Li <lzblack@users.noreply.github.com>
2023-03-27 16:02:41 -0400

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,5 +1,5 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
-from config import MAPBOX_API_KEY, MBTA_API_KEY
+from config import MAPBOX_TOKEN, MBTA_API_KEY
 
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
