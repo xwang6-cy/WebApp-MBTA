13833abaccef06dfb31ca3aafe7fa9922a531d9c
Zhi Li <lzblack@users.noreply.github.com>
2023-03-27 15:59:37 -0400

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,9 +1,9 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
-from config import MAPQUEST_API_KEY, MBTA_API_KEY
+from config import MAPBOX_API_KEY, MBTA_API_KEY
 
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
-MAPQUEST_BASE_URL = "http://mapquestapi.com/geocoding/v1/address"
+MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 
@@ -12,8 +12,7 @@ MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 def get_json(url):
     """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
+    Given a properly formatted URL for a JSON web API request, return a Python JSON object containing the response to that request.
 
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
@@ -22,20 +21,18 @@ def get_json(url):
 
 def get_lat_long(place_name):
     """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding API URL formatting requirements.
+    Given a place name or address, return a (latitude, longitude) tuple with the coordinates of the given place.
+
+    See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
     pass
 
 
 def get_nearest_station(latitude, longitude):
     """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
+    Given latitude and longitude strings, return a (station_name, wheelchair_accessible) tuple for the nearest MBTA station to the given coordinates.
+
+    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
     pass
 
